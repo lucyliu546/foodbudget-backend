b13812a8771535e83e74051a837cb26aca8a257c
lucyliu546@gmail.com <lucyliu546@gmail.com>
2020-09-30 21:24:52 -0500

    fileupdates

--- foodbudgetbackend/foodbudgetbackend/settings.py
+++ foodbudgetbackend/foodbudgetbackend/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'foodbudgetbackend.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'ebdb',
